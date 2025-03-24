f74d58015ca3f4779143bdc2f35cfdf11f134e25
Gustavo <163600766+gustavossdev@users.noreply.github.com>
2025-03-24 17:51:26 -0300

    melhorias na animação do menu e pequenos ajustes no layout do site

--- script/script.js
+++ script/script.js
@@ -1,4 +1,15 @@
-function toggleMenu() {
-    const navLinks = document.getElementById('nav-links');
-    navLinks.classList.toggle('active');
+let navbar = document.querySelector('#navbar');
+let menu = document.querySelector('#menu-icon');
+
+let close = menu.textContent;
+let open = "close";
+
+function toggleMenu(){
+    navbar.classList.toggle('open');
+
+    if (menu.textContent === close) {
+        menu.textContent = open;
+    } else{
+        menu.textContent = close;
+    }
 }
