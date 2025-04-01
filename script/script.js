ff1d6a5ff053b56c057f3e51631fe46c6562cc69
Gustavo <163600766+gustavossdev@users.noreply.github.com>
2025-04-01 16:59:29 -0300

    implementação darkmode

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+//Função para abrir e fechar Menu
 let navbar = document.querySelector('#navbar');
 let menu = document.querySelector('#menu-icon');
 
@@ -13,3 +14,25 @@ function toggleMenu(){
         menu.textContent = close;
     }
 }
+
+//Darkmode
+
+let darkmode = localStorage.getItem('darkmode');
+const themeSwitch = document.getElementById('theme-switch');
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkmode', 'active');
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkmode', null);
+}
+
+if(darkmode === "active") enableDarkmode();
+
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+})
